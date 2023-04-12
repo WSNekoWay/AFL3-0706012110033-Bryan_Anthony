c578abe869eef9da16b016d9770d2eb29f4c0d7e
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-12 09:34:45 +0700

    Section 4  Chapter 1

---
+++ AFL3-0706012110033-Bryan_Anthony/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  AFL3-0706012110033-Bryan_Anthony
+//
+//  Created by MacBook Pro on 12/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+          Image("turtlerock")
+              .clipShape(Circle())
+              .overlay {
+                  Circle().stroke(.white, lineWidth: 4)
+              }
+              .shadow(radius: 7)
+      }}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
