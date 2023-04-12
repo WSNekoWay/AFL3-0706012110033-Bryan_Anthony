2e8efded757dc30a06e186a8f1d36666b620e1df
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-12 09:36:10 +0700

    section 4 chapter 1

--- AFL3-0706012110033-Bryan_Anthony/CircleImage.swift
+++ AFL3-0706012110033-Bryan_Anthony/CircleImage.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-          Image("turtlerock")
+          Image("guardian")
               .clipShape(Circle())
               .overlay {
                   Circle().stroke(.white, lineWidth: 4)
               }
               .shadow(radius: 7)
+              
       }}
 
 struct CircleImage_Previews: PreviewProvider {
