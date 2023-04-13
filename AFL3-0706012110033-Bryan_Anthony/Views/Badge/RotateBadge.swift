9327688765094f27c66cca709108746006dbcd1e
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-14 00:07:45 +0700

    Draw the badge symbol

---
+++ AFL3-0706012110033-Bryan_Anthony/Views/Badge/RotateBadge.swift
@@ -0,0 +1,24 @@
+//
+//  RotateBadge.swift
+//  AFL3-0706012110033-Bryan_Anthony
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
