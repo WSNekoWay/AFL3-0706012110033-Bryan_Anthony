80a1e604fefb0d9b6b1a4e4d5362780c1f6319c8
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-14 10:24:04 +0700

    Add a navigation between section

--- AFL3-0706012110033-Bryan_Anthony/Views/ContentView.swift
+++ AFL3-0706012110033-Bryan_Anthony/Views/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 //This struct give view of LandmarkList
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    enum Tab {
+            case featured
+            case list
+        }
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+                    CategoryHome()
+                        .tabItem {
+                            Label("Featured", systemImage: "star")
+                        }
+                        .tag(Tab.featured)
+
+                    LandmarkList()
+                        .tabItem {
+                            Label("List", systemImage: "list.bullet")
+                        }
+                        .tag(Tab.list)
+        }
     }
 }
 
