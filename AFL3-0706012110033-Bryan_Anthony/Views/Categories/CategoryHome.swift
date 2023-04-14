124cc7c829f1a5be0698d4d5045af910c08c43d7
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-14 08:44:23 +0700

    Add Category View

---
+++ AFL3-0706012110033-Bryan_Anthony/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  AFL3-0706012110033-Bryan_Anthony
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+            }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
