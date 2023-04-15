6d0bc1cbd9f324bd495cd657c79f3abba212d057
WSNekoWay <banthony03@student.ciputra.ac.id>
2023-04-15 21:17:51 +0700

    Create a view to represent a UIPageViewController

---
+++ AFL3-0706012110033-Bryan_Anthony/Views/PageView/PageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PageViewController.swift
+//  AFL3-0706012110033-Bryan_Anthony
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+           let pageViewController = UIPageViewController(
+               transitionStyle: .scroll,
+               navigationOrientation: .horizontal)
+
+           return pageViewController
+       }
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+            pageViewController.setViewControllers(
+                [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        }
+    
+   }
